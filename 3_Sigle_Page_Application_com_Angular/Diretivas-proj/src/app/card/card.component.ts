b3cef4380744486a9faa58c5a0a5b2639e9015e0
Gustavo.Fernandes <gustavo.freitas.afel@gmail.com>
2023-08-09 17:11:39 -0300

    "Subi pra não perder"

---
+++ 3_Sigle_Page_Application_com_Angular/Diretivas-proj/src/app/card/card.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  produtos: string[] = []
+  menuType: string = "User"
+
+  constructor() {
+    this.produtos = [
+      "mouse",
+      "teclado",
+      "cabo",
+      "tela",
+      "fonte"
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+  adicionar(){
+    this.produtos.push("Felipe");
+  }
+
+  remover(index: number){
+    this.produtos.splice(index, 1)
+  }
+
+}
