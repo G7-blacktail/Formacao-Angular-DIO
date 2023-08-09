ec1d04469b4763c9fd61fc2278141e3fb68e0322
Gustavo.Fernandes <gustavo.freitas.afel@gmail.com>
2023-08-09 17:49:10 -0300

    "Chegando a criação de modulos"

--- 3_Sigle_Page_Application_com_Angular/Diretivas-proj/src/app/app.module.ts
+++ 3_Sigle_Page_Application_com_Angular/Diretivas-proj/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
